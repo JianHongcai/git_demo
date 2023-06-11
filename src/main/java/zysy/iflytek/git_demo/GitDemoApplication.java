2bba3a18861030169a11487fe9eb5fb0ae52710c
Junglesouljah <13595688964@163.com>
2023-06-11 11:35:31 +0800

    hot-fix submit1

--- src/main/java/zysy/iflytek/git_demo/GitDemoApplication.java
+++ src/main/java/zysy/iflytek/git_demo/GitDemoApplication.java
@@ -11,7 +11,7 @@ public class GitDemoApplication {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
-
+        System.out.println("4");
     }
 
 }
