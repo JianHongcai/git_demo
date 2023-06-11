9ccac593a967a5c9df0d43b11de67968eb61f38f
Junglesouljah <13595688964@163.com>
2023-06-11 11:21:31 +0800

    second submit

--- src/main/java/zysy/iflytek/git_demo/GitDemoApplication.java
+++ src/main/java/zysy/iflytek/git_demo/GitDemoApplication.java
@@ -9,6 +9,8 @@ public class GitDemoApplication {
     public static void main(String[] args) {
 //        SpringApplication.run(GitDemoApplication.class, args);
         System.out.println("1");
+        System.out.println("2");
+
     }
 
 }
